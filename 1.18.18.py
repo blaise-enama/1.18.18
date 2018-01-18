e59086889b1838ba9a55bd2a2cc4fd95ccc5b113
blaise-enama <34690993+blaise-enama@users.noreply.github.com>
2018-01-18 13:39:58 -0500

    Add files via upload

---
+++ 1.18.18.py
@@ -0,0 +1,45 @@
+
+#1. Put down 4 examples of different data types
+#integer
+10
+#float:
+10.0
+#Boolean:
+True or false 
+#String:
+"10"
+
+#2)
+(4<5) (5<6) #output: True
+
+#3)add all numbers from 0 to 100
+total= 0
+for number in range(100):
+    total= total + number
+    print(total) #Sum=4950
+
+#5)
+#input:
+'hello'=="Hello"
+True!= False
+42='42'
+myAge=29
+myAge>=15
+myAge==15
+#output:
+false
+True
+True
+
+#6)Write a program that displays all the even numbers in a range of 1 to 30
+total=1
+for number in range(31):
+    if number%2 == 0:
+        print(number,"is even")
+
+#7)Change the range above to every number above 15:
+total=1
+for number in range(15,31):
+    if number%2 ==0:
+        print(number, "is even") 
+
